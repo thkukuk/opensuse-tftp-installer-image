61f7019d1b1bd6510e140e1bcc6c39973a4f985a
Thorsten Kukuk <kukuk@thkukuk.de>
2018-07-12 15:45:36 +0200

    Add local IP address to dnsmasq.conf

--- opensuse-pxe-server-image/root/usr/local/bin/entrypoint.sh
+++ opensuse-pxe-server-image/root/usr/local/bin/entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 set -x
 
+ipaddr=$1
+
+if [ -n "$ipaddr" ]; then
+   sed -i "s|localaddr|$ipaddr|g" /etc/dnsmasq.conf
+else
+   echo "Please specify the local IP address of the host"
+fi
+
 exec dnsmasq --no-daemon
